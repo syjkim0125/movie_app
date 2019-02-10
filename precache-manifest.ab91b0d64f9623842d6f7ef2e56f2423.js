f144fc56d545179b7322083162be56cda4f286c5
kimheesun <heesun@kimheesunui-MacBook-Pro.local>
2019-02-10 20:40:10 +0900

    Updates

---
+++ precache-manifest.ab91b0d64f9623842d6f7ef2e56f2423.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8922085cb86658e80b5e",
+    "url": "/movie_app/static/css/main.fa9809ef.chunk.css"
+  },
+  {
+    "revision": "8922085cb86658e80b5e",
+    "url": "/movie_app/static/js/main.8922085c.chunk.js"
+  },
+  {
+    "revision": "da87d06f04d863a24d6d",
+    "url": "/movie_app/static/js/1.da87d06f.chunk.js"
+  },
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "179275b4da820d2ab1f1ab7eefd42813",
+    "url": "/movie_app/index.html"
+  }
+];
